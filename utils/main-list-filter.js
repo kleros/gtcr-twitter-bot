d09816e80038401e3bced69f839d745e1a1ecb28
Green <40367733+greenlucid@users.noreply.github.com>
2022-11-01 18:45:46 -0300

    feat: better main list filter

--- utils/main-list-filter.js
+++ utils/main-list-filter.js
@@ -1,15 +1,30 @@
 // not sure how to make this .ts, oh well
 const fetch = require('node-fetch')
 
-const containedInMain = async (subgraphEndpoint, main, tcr) => {
+const { toChecksumAddress } = require('ethereum-checksum-address')
+
+// just make the query work like this:
+// 1. check if list is in a list.
+// 2. if so, check if that list (take the 1st one) is also in a list.
+// 3. iterate 5 times.
+
+const getIncluderList = async (subgraphEndpoint, tcr) => {
   // not sure right now, if appears the data for both chain registries
   // starts with "0xd594". then, it predictably contains the address.
-  const targetData = `0xd594${tcr.slice(2)}`
+  const classicTargetData = `0xd594${tcr.slice(2)}`
   const subgraphQuery = {
     query: `
     {
-      items(where: {registry: "${main}", data: "${targetData}", status_in: [Registered, ClearingRequested]}) {
-        id
+      items(where: {data: "${classicTargetData}", status_in: [Registered, ClearingRequested]}) {
+        registryAddress
+      }
+
+      itemProps(where: {type: "GTCR address", value: "${toChecksumAddress(
+        tcr
+      )}"}) {
+        item {
+          registryAddress
+        }
       }
     }
     `
@@ -19,10 +34,38 @@ const containedInMain = async (subgraphEndpoint, main, tcr) => {
     body: JSON.stringify(subgraphQuery)
   })
 
-  const { data } = await response.json()
-  const isContained = data.items.length > 0
+  const classicRegistry =
+    response.data.items.length > 0
+      ? response.data.items[0].registryAddress
+      : undefined
 
-  return isContained
+  const lightRegistry =
+    response.data.itemProps > 0
+      ? response.data.itemProps[0].item.registryAddress
+      : undefined
+
+  if (classicRegistry) return classicRegistry
+  else if (lightRegistry) return lightRegistry
+
+  return null
+}
+
+const containedInMain = async (subgraphEndpoint, main, tcr) => {
+  let currentTcr = tcr.toLowerCase()
+  for (let i = 0; i < 5; i++) {
+    const containerRegistry = await getIncluderList(
+      subgraphEndpoint,
+      currentTcr
+    )
+    if (containerRegistry === null) return false
+    if (containerRegistry.toLowerCase() === main.toLowerCase()) return true
+
+    currentTcr = containerRegistry.toLowerCase()
+  }
+
+  // probably a loop occurred, lets escape.
+  // sorry i didn't bother to implement dijsktra
+  return false
 }
 /**
  * Checks if a tcr is contained in the main registry, or if it's the main registry.
